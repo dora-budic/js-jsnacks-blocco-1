4cc67a5cf6cca787f997dd1795ef28bc919824a1
Dora Budić <dbudic0706@gmail.com>
2021-03-03 16:24:33 +0100

    Exercise 8

---
+++ jsnack8/main.js
@@ -0,0 +1,14 @@
+// Chiedi un numero di 4 cifre all’utente e calcola la somma
+// di tutte le cifre che compongono il numero.
+
+var number = prompt("Write a 4 digit number:");
+
+console.log(number);
+
+var sum = 0;
+
+for (var i = 0; i < number.length; i++) {
+  var sum = sum + parseInt(number.charAt(i));
+}
+
+document.getElementById('sum').innerHTML = "Sum of figures in number " + number + " is: " + sum;
